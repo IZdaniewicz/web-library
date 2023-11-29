6da01545f020400087b607388188b5ba9cff1251
kmaliszewski <kuba.maliszewski01@gmail.com>
2023-11-29 23:28:21 +0100

    add migrations for entities

--- web-library/Book/Entity/Book.cs
+++ web-library/Book/Entity/Book.cs
@@ -6,44 +6,15 @@ using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace web_library.Book.Entity;
 
-[Table("books")]
 public class Book
 {
-    [Column(name: "id")]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-
-    [Column(name: "isbn")]
     public string ISBN { get; set; }
-
-    [Column(name: "title")]
     public string Title { get; set; }
-
-    [Column(name: "author")]
     public string Author { get; set; }
-
-    [Column(name: "publisher")]
     public string Publisher { get; set; }
-
-    [Column(name: "publication_date")]
     public DateOnly Publication_date{ get; set; }
-
-    [Column(name: "location")]
     public string Location { get; set; }
-
-    [Column(name: "description")]
     public string Description { get; set; }
-
     public ICollection<BookCopy> Copies { get; } = new List<BookCopy>();
-
-    public Book(string iSBN, string title, string author, string publisher, DateOnly publication_date, string location, string description = "")
-    {
-        ISBN = iSBN;
-        Title = title;
-        Author = author;
-        Publisher = publisher;
-        Publication_date = publication_date;
-        Location = location;
-        Description = description;
-    }
 }
--- web-library/Book/Entity/BookCopy.cs
+++ web-library/Book/Entity/BookCopy.cs
@@ -1,21 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿namespace web_library.Book.Entity;
 
-namespace web_library.Book.Entity;
-
-[Table("books_copies")]
 public class BookCopy
 {
-    [Column(name: "id")]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-
-    [Column(name: "book_id")]
     public int BookId { get; set; }
-    public Book Book { get; set; }
-
-    public BookCopy(int bookId)
-    {
-        BookId = bookId;
-    }
+    public Book Book { get; set; } = new Book();
 }
 
